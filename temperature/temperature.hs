f272e1edede9415df8f92d047c6cf5de272c7fac
mr-fool <mr-fool@users.noreply.github.com>
2015-01-26 23:32:14 -0700

    Update temperature.hs

--- temperature/temperature.hs
+++ temperature/temperature.hs
@@ -3,20 +3,6 @@ main = do
     temperature <- getLine
     let celsius = read temperature :: Double
     let cels2fahr a = a * 1.8 +32 
-    cels2fahr celsius
-    putStrLn "something"
-    --let result = show (cels2fahr celsius)
-    --putStrLn ("The temperature in fahrenheit " ++ result)
+    let result = show (cels2fahr celsius)
+    putStrLn ("The temperature in fahrenheit " ++ result)
   
-
-  
--{
-ghc --make "temperature.hs" (in directory: /home/mr-fool/Documents/haskell/temperature)
-[1 of 1] Compiling Main             ( temperature.hs, temperature.o )
-temperature.hs:6:15:
-    Couldn't match expected type ‘IO a0’ with actual type ‘Double’
-    In the first argument of ‘cels2fahr’, namely ‘celsius’
-    In a stmt of a 'do' block: cels2fahr celsius
-Compilation failed.
-
-}-
