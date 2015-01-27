6354859a67d0f8a46a43706ed1825580d7babd8e
mr-fool <mr-fool@users.noreply.github.com>
2015-01-26 23:30:01 -0700

    Update temperature.hs

--- temperature/temperature.hs
+++ temperature/temperature.hs
@@ -1,18 +1,22 @@
 main = do
     putStrLn "Please enter the temperature in celsius"
     temperature <- getLine
-    let celsius = read temperature :: Int
+    let celsius = read temperature :: Double
     let cels2fahr a = a * 1.8 +32 
-    let result = show (cels2fahr celsius)
-    putStrLn ("The temperature in fahrenheit " ++ result)
+    cels2fahr celsius
+    putStrLn "something"
+    --let result = show (cels2fahr celsius)
+    --putStrLn ("The temperature in fahrenheit " ++ result)
+  
+
   
 -{
 ghc --make "temperature.hs" (in directory: /home/mr-fool/Documents/haskell/temperature)
 [1 of 1] Compiling Main             ( temperature.hs, temperature.o )
-temperature.hs:6:24:
-    No instance for (Fractional Int) arising from a use of ‘cels2fahr’
-    In the first argument of ‘show’, namely ‘(cels2fahr celsius)’
-    In the expression: show (cels2fahr celsius)
-    In an equation for ‘result’: result = show (cels2fahr celsius)
+temperature.hs:6:15:
+    Couldn't match expected type ‘IO a0’ with actual type ‘Double’
+    In the first argument of ‘cels2fahr’, namely ‘celsius’
+    In a stmt of a 'do' block: cels2fahr celsius
 Compilation failed.
+
 }-
