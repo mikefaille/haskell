87110f97d5f34fcf8be6d8d56572794e20aa1acd
mr-fool <mr-fool@users.noreply.github.com>
2015-02-04 10:36:38 -0700

    Update random.hs

--- random/random.hs
+++ random/random.hs
@@ -1,10 +1,20 @@
 import System.Random
 
 main = do
-  let rs = randomRs ('1', '8') g
-  print (take 10 rs); let (a, rs') = rs
+  g <- getStdGen
+  let (a:rs') = randomRs ('1', '8') g
+  print (take 10 rs)
+  let (a, rs') = rs
   putStrLn (show a) {- do more stuff with rs' here -}
 --randomRIO (0,6) :: Int >>= print
 -{
-random.hs:4:32: Not in scope: ‘g’
+ghc --make "random.hs" (in directory: /home/mr-fool/Documents/haskell/random)
+[1 of 1] Compiling Main             ( random.hs, random.o )
+random.hs:6:18:
+    Not in scope: ‘rs’
+    Perhaps you meant ‘rs'’ (line 5)
+random.hs:7:18:
+    Not in scope: ‘rs’
+    Perhaps you meant ‘rs'’ (line 7)
+Compilation failed.
 -}
