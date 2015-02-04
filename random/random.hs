639cd507acf30438b18f715bb2ead0d7dbce2d81
mr-fool <mr-fool@users.noreply.github.com>
2015-02-04 10:50:17 -0700

    Update random.hs

--- random/random.hs
+++ random/random.hs
@@ -2,11 +2,13 @@ import System.Random
 
 main = do
   g <- getStdGen
-  let {rs = randomRs ('1', '8') g}
+  let generator = mkStdGen 0
+  let {rs = randomRs ('1', '8') generator}
   print (take 10 rs)
   let {(a:rs') = rs}
   putStrLn (show a)
   putStrLn (show a)
-"2532653565"
-'2'
-'2'
+  {-r <- randomRIO (0,6)
+  print r
+  r2 <- randomRIO (7,9)
+  print r2 -}
