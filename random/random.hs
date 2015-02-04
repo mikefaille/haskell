64c3f10a2ae83ae0cf62f476409ee90cb2e86147
mr-fool <mr-fool@users.noreply.github.com>
2015-02-04 11:05:50 -0700

    Update random.hs

--- random/random.hs
+++ random/random.hs
@@ -1,17 +1,8 @@
 import System.Random
 
 main = do
-  {-g <- getStdGen
-  let generator = mkStdGen 0
-  let {rs = randomRs ('1', '8') generator}
-  print (take 10 rs)
-  let {(a:rs') = rs}
-  let (a, g) = next generator 
-  print a
-  let (a, g) = next g 
-  print a
-  putStrLn (show a)-}
   let randomize = randomRIO (0, 6) :: IO Int
   r <- randomize
   print r
+  r <- randomize
   print r
