b1e3be021b938436a9b26e1051cbd8a3fcd72850
mr-fool <mr-fool@users.noreply.github.com>
2015-02-04 10:21:57 -0700

    Update random.hs

--- random/random.hs
+++ random/random.hs
@@ -2,47 +2,35 @@ import System.Random
 
 main = do
   g <- getStdGen
-  print $ take 10 (randomRs (1, 8) g)
+  print $ take 10 (randomRs ('1', '8') g)
+  let a = randomRs ('1', '8')
+  putStrLn (show(a) )
 -{
 ghc --make "random.hs" (in directory: /home/mr-fool/Documents/haskell/random)
 [1 of 1] Compiling Main             ( random.hs, random.o )
-random.hs:5:3:
-    No instance for (Show a0) arising from a use of ‘print’
-    The type variable ‘a0’ is ambiguous
-    Note: there are several potential instances:
-      instance Show StdGen -- Defined in ‘System.Random’
-      instance Show Double -- Defined in ‘GHC.Float’
-      instance Show Float -- Defined in ‘GHC.Float’
-      ...plus 65 others
-    In the expression: print
-    In a stmt of a 'do' block: print $ take 10 (randomRs (1, 8) g)
+random.hs:6:11:
+    No instance for (RandomGen g0) arising from a use of ‘randomRs’
+    The type variable ‘g0’ is ambiguous
+    Relevant bindings include
+      a :: g0 -> [Char] (bound at random.hs:6:7)
+    Note: there is a potential instance available:
+      instance RandomGen StdGen -- Defined in ‘System.Random’
+    In the expression: randomRs ('1', '8')
+    In an equation for ‘a’: a = randomRs ('1', '8')
     In the expression:
       do { g <- getStdGen;
-           print $ take 10 (randomRs (1, 8) g) }
-random.hs:5:20:
-    No instance for (Random a0) arising from a use of ‘randomRs’
-    The type variable ‘a0’ is ambiguous
-    Note: there are several potential instances:
-      instance Random Bool -- Defined in ‘System.Random’
-      instance Random Foreign.C.Types.CChar -- Defined in ‘System.Random’
-      instance Random Foreign.C.Types.CDouble
-        -- Defined in ‘System.Random’
-      ...plus 33 others
-    In the second argument of ‘take’, namely ‘(randomRs (1, 8) g)’
-    In the second argument of ‘($)’, namely
-      ‘take 10 (randomRs (1, 8) g)’
-    In a stmt of a 'do' block: print $ take 10 (randomRs (1, 8) g)
-random.hs:5:30:
-    No instance for (Num a0) arising from the literal ‘1’
-    The type variable ‘a0’ is ambiguous
-    Note: there are several potential instances:
-      instance Num Double -- Defined in ‘GHC.Float’
-      instance Num Float -- Defined in ‘GHC.Float’
-      instance Integral a => Num (GHC.Real.Ratio a)
-        -- Defined in ‘GHC.Real’
-      ...plus 37 others
-    In the expression: 1
-    In the first argument of ‘randomRs’, namely ‘(1, 8)’
-    In the second argument of ‘take’, namely ‘(randomRs (1, 8) g)’
+           print $ take 10 (randomRs ('1', '8') g);
+           let a = randomRs ...;
+           putStrLn (show (a)) }
+random.hs:7:13:
+    No instance for (Show (g0 -> [Char])) arising from a use of ‘show’
+    In the first argument of ‘putStrLn’, namely ‘(show (a))’
+    In a stmt of a 'do' block: putStrLn (show (a))
+    In the expression:
+      do { g <- getStdGen;
+           print $ take 10 (randomRs ('1', '8') g);
+           let a = randomRs ...;
+           putStrLn (show (a)) }
 Compilation failed.
+
 -}
