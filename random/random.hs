6b42822d062ef3ace21e0d3a81f62371b5fff622
mr-fool <mr-fool@users.noreply.github.com>
2015-02-04 10:26:31 -0700

    Update random.hs

--- random/random.hs
+++ random/random.hs
@@ -4,3 +4,10 @@ main = do
   g <- getStdGen
   let a = take 1 (randomRs ('1', '8') g)
   putStrLn (show(a) )
+  let b = take 1 (randomRs ('1', '8') g)
+  putStrLn (show(b) )
+-{
+runhaskell random.hs
+"7"
+"7"
+-}
