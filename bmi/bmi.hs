1151a0e9841e7f63efd1bac6e8d9617e09abb8fd
mr-fool <mr-fool@users.noreply.github.com>
2015-02-09 15:52:21 -0700

    Update bmi.hs

--- bmi/bmi.hs
+++ bmi/bmi.hs
@@ -1,3 +1,10 @@
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal.. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some wieght, fatty!"
+    | otherwise = "You're a whale, congratulations!"
+    where bmi = weight / height ** 2
 main = do
     putStrLn "Please enter your weight"
     weight <- getLine
@@ -7,19 +14,20 @@ main = do
     
     let numberWeight = read weight
     let numberHeight  = read height
-    
+    {-
     let bmiTell :: (RealFloat a) => a -> a -> String
         bmiTell weight height
             | bmi <= 18.5 = "You're underweight, you emo, you!"
             | bmi <= 25.0 = "You're supposedly normal.. Pffft, I bet you're ugly!"
             | bmi <= 30.0 = "You're fat! Lose some wieght, fatty!"
             | otherwise = "You're a whale, congratulations!"
-            where bmi = weight / height ** 2
+            where bmi = weight / height ** 2 -}
     bmiTell numberWeight numberHeight
-{- 
+
+{-
 ghc --make "bmi.hs" (in directory: /home/mr-fool/Documents/haskell/bmi)
 [1 of 1] Compiling Main             ( bmi.hs, bmi.o )
-bmi.hs:18:5:
+bmi.hs:25:5:
     Couldn't match type ‘[]’ with ‘IO’
     Expected type: IO Char
       Actual type: String
@@ -31,4 +39,5 @@ bmi.hs:18:5:
            height <- getLine;
            .... }
 Compilation failed.
+
 -}
