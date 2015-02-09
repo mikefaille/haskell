07b8cf9afa45346e545e92cf64ae96b23137e5ce
mr-fool <mr-fool@users.noreply.github.com>
2015-02-09 15:42:36 -0700

    Update bmi.hs

--- bmi/bmi.hs
+++ bmi/bmi.hs
@@ -8,12 +8,18 @@ main = do
     let numberWeight = read weight
     let numberHeight  = read height
     
-    bmiTell :: (RealFloat a) => a -> a -> String
-    bmiTell weight height
+    let bmiTell :: (RealFloat a) => a -> a -> String
+    let bmiTell weight height
         | bmi <= 18.5 = "You're underweight, you emo, you!"
         | bmi <= 25.0 = "You're supposedly normal.. Pffft, I bet you're ugly!"
         | bmi <= 30.0 = "You're fat! Lose some wieght, fatty!"
         | otherwise = "You're a whale, congratulations!"
         where bmi = weight / height ^2
     bmiTell numberWeight numberHeight
--- bmi.hs:13:9: parse error on input ‘|’
+{-
+ghc --make "bmi.hs" (in directory: /home/mr-fool/Documents/haskell/bmi)
+[1 of 1] Compiling Main             ( bmi.hs, bmi.o )
+bmi.hs:13:9:
+    parse error (possibly incorrect indentation or mismatched brackets)
+Compilation failed.
+-}
