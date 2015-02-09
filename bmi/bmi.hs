30fc8d1404698a04af1ba7d71375ac9fb7030834
mr-fool <mr-fool@users.noreply.github.com>
2015-02-09 15:32:36 -0700

    Create bmi.hs

---
+++ bmi/bmi.hs
@@ -0,0 +1,19 @@
+main = do
+    putStrLn "Please enter your weight"
+    weight <- getLine
+    
+    putStrLn "Please enter your height"
+    height <- getLine    
+    
+    let numberWeight = read weight
+    let numberHeight  = read height
+    
+    bmiTell :: (RealFloat a) => a -> a -> String
+    bmi weight height
+        | bmi <= 18.5 = "You're underweight, you emo, you!"
+        | bmi <= 25.0 = "You're supposedly normal.. Pffft, I bet you're ugly!"
+        | bmi <= 30.0 = "You're fat! Lose some wieght, fatty!"
+        | otherwise = "You're a whale, congratulations!"
+        where bmi = weight / height ^2
+    bmiTell numberWeight numberHeight
+-- bmi.hs:13:9: parse error on input ‘|’
