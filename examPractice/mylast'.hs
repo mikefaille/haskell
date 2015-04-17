fc7a674f6046929b8e74a68e5e78f92ea1567967
mr-fool <mr-fool@users.noreply.github.com>
2015-04-17 14:11:43 -0600

    Update mylast'.hs

--- examPractice/mylast'.hs
+++ examPractice/mylast'.hs
@@ -1,13 +1,4 @@
 mylast' :: [a] -> a
-mylast' [] = error "no empty lists allow"
-mylast' (_:y) = y
-Prelude> :load mylast.hs
-[1 of 1] Compiling Main             ( mylast.hs, interpreted )
-
-mylast.hs:3:17:
-    Couldn't match expected type `a' with actual type `[a]'
-      `a' is a rigid type variable bound by
-          the type signature for mylast' :: [a] -> a at mylast.hs:1:12
-    In the expression: y
-    In an equation for mylast': mylast' (_ : y) = y
-Failed, modules loaded: none.
+mylast' [] = error "empty list not cool"
+mylast' (x:[]) = x
+mylast' (x:y) = mylast' y
